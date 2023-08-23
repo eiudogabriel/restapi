6d22f614179d2dffa8a22c4bf4639b29d68615d8
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-23 17:50:48 -0300

    Mongoose setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,34 @@
-console.log("hello!");
+import express from "express";
+import http from "http";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
+
+dotenv.config({ path: "local.env" });
+
+const url = process.env.MONGO_URL;
+
+mongoose.Promise = Promise;
+mongoose.connect(url);
+mongoose.connection.on("error", (error: Error) => console.log(error));
