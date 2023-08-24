a8f6f6b7974faea351e04ce95dfeea8020827c9c
Eiudo Gabriel <eiudogabriel@gmail.com>
2023-08-23 21:25:29 -0300

    small fix

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -5,7 +5,7 @@ import { getUserBySessionToken } from '../db/users';
 
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const sessionToken = req.cookies('EIUDO-AUTH');
+        const sessionToken = req.cookies['EIUDO-AUTH'];
 
         if (!sessionToken) {
             return res.sendStatus(403);
